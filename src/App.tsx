4b297dab18bad4eb5ad3d7b74385b3c02331141c
bluelf45 <oscar.acevedob@usm.cl>
2024-11-04 18:13:44 -0300

    Binding elements bug removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "leaflet/dist/leaflet.css";
 import './App.css';
 import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 
-const MapView = ({ lat, lng }) => {
+const MapView = ({ lat, lng }:{lat:any, lng:any})  => {
   const map = useMap();
   map.setView([lat, lng], map.getZoom()); // Set view to new coordinates
   return <Marker position={[lat, lng]} />;
@@ -17,7 +17,7 @@ function App() {
   const [lat, setLat] = useState(51.505);
   const [lng, setLng] = useState(-0.09);
 
-  const handleSearch = async (e) => {
+  const handleSearch = async (e: React.ChangeEvent<any>) => {
     e.preventDefault();
     
     try {
@@ -47,36 +47,38 @@ function App() {
       <div className="bg-desktop bg-cover p-10 relative z-10">
         <p className="text-white text-3xl font-semibold flex justify-evenly">IP Address Tracker</p>
         
-        <div className="mt-4">
-          <form className="max-w-md mx-auto" onSubmit={handleSearch}>
+        <div className="mt-4 relative">
+          <form className="max-w-md mx-auto flex" onSubmit={handleSearch}>
             <input 
               type="search" 
               value={ip} 
               onChange={(e) => setIp(e.target.value)} 
               id="default-search" 
-              className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500" 
+              className="p-4 w-full text-sm text-gray-900 border border-gray-300 rounded-l bg-gray-50 focus:ring-blue-500" 
               placeholder="Search..."
             />
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 rounded-lg text-sm px-4 py-2">Search</button>
+            <button type="submit" className="text-white bg-black  hover:invert rounded-r text-sm px-4 py-2">
+            <img className=" p-1" src='src\assets\images\icon-arrow.svg' />
+            </button>
           </form>
         </div>
-        <div className="w-3/5 mx-auto mt-10">
-          <div className="py-8 grid desktop:grid-cols-4 desktop:divide-x-2 desktop:divide-y-0 mobile:grid-cols-1 mobile:divide-y-2 bg-white rounded">
-            <div className="px-4 text-center">
-              <p className="text-slate-600 text-xl">IPAddress</p>
-              <p className="text-2xl mt-2 font-semibold">{ip}</p>
+        <div className="desktop:w-3/5 mobile:w-3/6 mx-auto mt-10">
+          <div className="py-8 grid desktop:grid-cols-4 desktop:divide-x-2 mobile:grid-cols-1  bg-white rounded">
+            <div className="px-4 text-center mobile:mt-0 desktop:mt-0">
+              <p className="text-slate-600 font-semibold text-xl">IPAddress</p>
+              <p className="text-2xl mt-2 font-bold">{ip}</p>
             </div>
-            <div className="px-4 text-center">
-              <p className="text-slate-600 text-xl">Location</p>
-              <p className="text-2xl mt-2 font-semibold">{location}</p>
+            <div className="px-4 text-center mobile:mt-4 desktop:mt-0">
+              <p className="text-slate-600 font-semibold text-xl">Location</p>
+              <p className="text-2xl mt-2 font-bold">{location}</p>
             </div>
-            <div className="px-4 text-center">
-              <p className="text-slate-600 text-xl">Timezone</p>
-              <p className="text-2xl mt-2 font-semibold">{timezone}</p>
+            <div className="px-4 text-center mobile:mt-4 desktop:mt-0">
+              <p className="text-slate-600 font-semibold text-xl">Timezone</p>
+              <p className="text-2xl mt-2 font-bold">{timezone}</p>
             </div>
-            <div className="px-4 text-center">
-              <p className="text-slate-600 text-xl">ISP</p>
-              <p className="text-2xl mt-2 font-semibold">{isp}</p>
+            <div className="px-4 text-center mobile:mt-4 desktop:mt-0">
+              <p className="text-slate-600 font-semibold text-xl">ISP</p>
+              <p className="text-2xl mt-2 font-bold">{isp}</p>
             </div>
           </div>
         </div>
